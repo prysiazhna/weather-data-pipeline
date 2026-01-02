55c327a4a6694a966967f98284c5b6e25bdc34ae
olhaprysiazhna <olha.prysiazhna@gmail.com>
2026-01-02 20:45:03 +0200

    gitignore

--- dbt/weather_dbt/target/compiled/weather_dbt/models/marts/mart_weekly_change.sql
+++
@@ -1,6 +0,0 @@
-select
-  location_id,
-  date,
-  temp_avg_c,
-  temp_avg_c - lag(temp_avg_c, 7) over (partition by location_id order by date) as temp_wow_change
-from "weather"."analytics_core"."fact_weather_daily"
